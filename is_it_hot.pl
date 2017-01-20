54a94a8c91c30111f8cc416d2b75cf3b1d6c6317
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-01-20 17:40:45 +0200

    Initial commit

---
+++ is_it_hot.pl
@@ -0,0 +1,24 @@
+conv_temp(X) :-
+    Temp is 9.0/5.0*X+32,
+    is_it_hot(Temp).
+
+is_it_hot(T) :-
+    T > 90,
+    write("Hot temperature today."),
+    nl,
+	get_input().
+
+is_it_hot(T) :-
+    T < 30,
+    write("It is cold today"),
+    nl,
+	get_input().
+
+is_it_hot(T) :-
+    get_input().
+
+get_input() :-
+    write("Please type the temperature in Celcius: "),
+    nl,
+    read(X),
+    conv_temp(X).
---
+++ set.pl
@@ -0,0 +1,24 @@
+% An empty list is a set.
+set([], []).
+
+% Put the head in the result,
+% remove all occurrences of the head from the tail,
+% make a set out of that.
+set([H|T], [H|T1]) :- 
+    remv(H, T, T2),
+    set(T2, T1).
+
+% Removing anything from an empty list yields an empty list.
+remv(_, [], []).
+
+% If the head is the element we want to remove,
+% do not keep the head and
+% remove the element from the tail to get the new list.
+remv(X, [X|T], T1) :- remv(X, T, T1).
+
+% If the head is NOT the element we want to remove,
+% keep the head and
+% remove the element from the tail to get the new tail.
+remv(X, [H|T], [H|T1]) :-
+    X \= H,
+    remv(X, T, T1).
